f962ce557a31fd9f1ae1d87fecf70a08fe4f5722
EC <qin02qin@gmail.com>
2024-07-22 00:28:55 +0800

    add db init script

---
+++ scripts/schema.sql
@@ -0,0 +1,12 @@
+use db;
+ 
+CREATE TABLE restaurant (
+    user_id VARCHAR(64) NOT NULL,
+    restaurant_name VARCHAR(64) NOT NULL,
+    session_id VARCHAR(64) NOT NULL,
+    PRIMARY KEY (user_id , restaurant_name , session_id)
+);
+CREATE TABLE session (
+    uuid VARCHAR(64) PRIMARY KEY,
+    user_id VARCHAR(64) NOT NULL
+);
